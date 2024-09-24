b204418f384e369db035f8b239d38d2647b60bec
laughter1998 <laughter98@hanmail.net>
2024-09-24 10:37:13 +0900

    tailwind 적용

---
+++ mobile/yedaham_tablet_new/js/tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          ccc: '#ccc',
+          red: '#ea1919',
+          blue: '#003fff',
+          blueMain: '#213F9A',
+          blueLight: '#0085ff',
+          333: '#333',
+          666: '#666',
+          777: '#777',
+          ddd: '#ddd',
+        },
+      },
+    },
+  };
+  
