499bf6433fa3cd2e6f0219947020e21c5f81e34f
laughter1998 <laughter98@hanmail.net>
2024-09-25 09:30:34 +0900

    의전 ~16

--- mobile/yedaham_tablet_new/js/tailwind-config.js
+++ mobile/yedaham_tablet_new/js/tailwind-config.js
@@ -4,15 +4,22 @@ tailwind.config = {
         colors: {
           ccc: '#ccc',
           red: '#ea1919',
+          redOpa1: 'rgba(255, 125, 218, 0.1)',
+          redOpa2: 'rgba(255, 125, 218, 0.2)',
           blue: '#003fff',
           blueMain: '#213F9A',
           blueLight: '#0085ff',
+          blueOpa1: 'rgba(0, 133, 255, 0.1)',
+          blueOpa2: 'rgba(0, 133, 255, 0.2)',
+          yellowOpa1: ' rgba(255, 168, 0, 0.2);',
           333: '#333',
           666: '#666',
           777: '#777',
           ddd: '#ddd',
         },
+        fontSize: {
+          '2xl': '32px',
+        }
       },
     },
   };
-  
