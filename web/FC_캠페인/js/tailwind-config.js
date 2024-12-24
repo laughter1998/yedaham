465eddff97d42bb576db28df4a30510c9903f1c0
laughter1998 <laughter98@hanmail.net>
2024-12-24 15:30:08 +0900

    FC 캠페인

---
+++ web/FC_캠페인/js/tailwind-config.js
@@ -0,0 +1,42 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        gray333: "#333333",
+        gray666: "#666666",
+        gray999: "#999999",
+        grayCCC: "#CCCCCC",
+        grayDDD: "#DDDDDD",
+        grayAAA: "#AAAAAA",
+        grayD9: "#D9D9D9",
+        blue214: "#21409A",
+        blue779: "#7798FF",
+        blue275: "#2758EB",
+        blue769: "#7698ff",
+        blueDF: "#DFEEFF",
+        grayF1: "#F1F1F1",
+        gray1E: "#1E1E1E",
+        redED: "#ED1B2F",
+
+      },
+      boxShadow: {
+        card: '0 4px 20px 0 rgba(39, 88, 235, 0.2)',
+        input: 'inset 0 4px 4px 0 rgba(0, 0, 0, 0.1)',
+        button: '2px 2px 4px 0 rgba(0, 0, 0, 0.15)'
+      },
+      fontFamily: {
+        sans: [
+          'NanumSquareNeoBold',
+          'sans-serif',
+        ],
+        nanumSquare300: ['NanumSquareNeoLight', 'sans-serif'],
+        nanumSquare400: ['NanumSquareNeo', 'sans-serif'],
+        nanumSquare700: ['NanumSquareNeoBold', 'sans-serif'],
+        nanumSquare800: ['NanumSquareNeoExtraBold', 'sans-serif'],
+        nanumSquare900: ['NanumSquareNeoHeavy', 'sans-serif'],
+      },
+
+    },
+  },
+};
+
